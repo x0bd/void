2bb2a2b9aabc46f45eec3dcbce60570d4f77ed87
Tinodaishe Tembo <synthmancer@gmail.com>
2024-01-26 18:17:08 +0200

    style settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	integrations: [tailwind(), sitemap()],
+	site: SITE_URL,
+	markdown: {
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			theme: "rose-pine",
+			wrap: false,
+		},
+	},
 });
